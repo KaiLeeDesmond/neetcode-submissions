34b0db2bd1ebfe67d37dc97a65d0159018b76fcb
Amogh Krishna <72075341+KaiLeeDesmond@users.noreply.github.com>
2026-07-01 22:13:24 -0400

    Add: is-anagram - submission-15

---
+++ Data Structures & Algorithms/is-anagram/submission-15.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        hmap = {}
+        if len(s) != len(t):
+            return False
+        for c in s:
+            hmap[c] = hmap.get(c,0)+1
+        
+        for c in t:
+            if c not in hmap or hmap[c] == 0:
+                return False
+            hmap[c]-=1
+        
+        for v in hmap.values():
+            if v!=0:
+                return False
+            return True
+        return False
+            
